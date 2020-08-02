36e3ddeeaa240731609dd3b00d04c0fed9a237fd
SurajSingh01 <singhsurajd03@gmail.com>
2020-08-02 16:45:45 +0530

    Added : Reset functionality for the game to being fresh

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,3 +2,16 @@
 #!/bin/bash -x
 echo "Welcome to Tic Tac Toe Game"
 
+function reset()
+{
+	Array=(. . . . . . . . .)
+	player=1
+	gameStatus=1
+	echo "--------------------"
+	echo "Game is being Reset"
+	echo "--------------------"
+}
+
+reset
+
+
